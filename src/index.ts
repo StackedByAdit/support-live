75bbe4cfd773bba2c4418846545f9bbe34066f99
Aditya <adityaguptavns1@gmail.com>
2026-07-08 17:17:41 +0530

    add missing existing user logic

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,48 @@ const app = express();
 
 app.use(express.json());
 
-console.log(process.env.DATABASE_URL);
-
-
 app.post("/auth/signup", async (req: Request, res: Response) => {
-  const result = userSchema.safeParse(req.body);
-
-  if (!result.success) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid request schema",
-    });
-  }
-
-  const { name, email, password, role, supervisorId } = result.data;
-
-  if (role === "agent" && !supervisorId) {
-    return res.status(400).json({
-      success: false,
-      message: "supervisorId required for agent role",
-    });
-  }
-
   try {
+    const result = userSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid request schema",
+      });
+    }
+
+    const { name, email, password, role, supervisorId } = result.data;
+
+    if (role === "agent") {
+      if (!supervisorId) {
+        return res.status(400).json({
+          success: false,
+          message: "supervisorId required for agent role",
+        });
+      }
+
+      const supervisor = await prisma.user.findUnique({
+        where: {
+          id: supervisorId,
+        },
+      });
+
+      if (!supervisor) {
+        return res.status(404).json({
+          success: false,
+          message: "Supervisor not found",
+        });
+      }
+
+      if (supervisor.role !== "supervisor") {
+        return res.status(400).json({
+          success: false,
+          message: "User is not a supervisor",
+        });
+      }
+    }
+
     const existing = await prisma.user.findUnique({
       where: { email },
     });
@@ -48,7 +67,7 @@ app.post("/auth/signup", async (req: Request, res: Response) => {
     if (existing) {
       return res.status(409).json({
         success: false,
-        message: "Email already exists",
+        error: "Email already exists",
       });
     }
 
@@ -77,36 +96,41 @@ app.post("/auth/signup", async (req: Request, res: Response) => {
     return res.status(500).json({
       success: false,
       message: "Internal server error",
+      error: err,
     });
   }
 });
 
-app.post("/auth/login", async (req : Request, res : Response) => {
-
-}) 
-
-app.get("/auth/me", async (req : Request, res : Response) => {
-
-}) 
-
-app.post("/conversations", async (req : Request, res : Response) => {
-
-}) 
-
-app.post("/conversations/:id/assign", async (req : Request, res : Response) => {
-
-}) 
-
-app.post("/conversations/:id", async (req : Request, res : Response) => {
-
-}) 
-
-app.post("/conversations/:id/close", async (req : Request, res : Response) => {
+app.post("/auth/login", async (req: Request, res: Response) => {
 
 })
 
-app.get("/admin/analytics", async (req : Request, res : Response) => {
+app.get("/auth/me", async (req: Request, res: Response) => {
 
 })
+
+app.post("/conversations", async (req: Request, res: Response) => {
+
+})
+
+app.post("/conversations/:id/assign", async (req: Request, res: Response) => {
+
+})
+
+app.post("/conversations/:id", async (req: Request, res: Response) => {
+
+})
+
+app.post("/conversations/:id/close", async (req: Request, res: Response) => {
+
+})
+
+app.get("/admin/analytics", async (req: Request, res: Response) => {
+
+})
+
+app.listen(3000, () => {
+  console.log("running on port 3000")
+});
 
 
