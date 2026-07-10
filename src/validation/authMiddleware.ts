c42dcac552076ebadeef2accb567a3df820145e1
Aditya <adityaguptavns1@gmail.com>
2026-07-10 22:18:34 +0530

    make meta data passable

--- src/validation/authMiddleware.ts
+++ src/validation/authMiddleware.ts
@@ -1,46 +1,52 @@
-import type {Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt, { type JwtPayload } from "jsonwebtoken"
 import type { UUID } from "node:crypto";
 import { success } from "zod";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export interface CustomRequest extends Request{
-    userId : string;
-    name : string;
-    role : string;
-    email : string
-    supervisorId : UUID
+export interface CustomRequest extends Request {
+    userId: string;
+    name: string;
+    role: string;
+    email: string
+    supervisorId?: UUID
 }
 
 
 
-export function authMiddleware(req: CustomRequest, res : Response, next : NextFunction){
+export function authMiddleware(req: CustomRequest, res: Response, next: NextFunction) {
 
     try {
         const token = req.headers.authorization;
-    
-        if(!token){
+
+        if (!token) {
             return res.status(400).json({
-    
+
                 success: false,
-                message : "token missing"
+                message: "token missing"
             })
         }
         const verified = jwt.verify(token, JWT_SECRET) as JwtPayload;
 
         console.log(verified);
-    
+
         req.userId = verified.id;
         req.name = verified.name;
+        req.email = verified.email;
+        req.role = verified.role;
 
+        if (verified.supervisorId) {
+            req.supervisorId = verified.supervisorId as UUID;
+        }
+        
         next();
 
-    } catch (e){
+    } catch (e) {
 
         return res.status(400).json({
-            success : false,
-            message : "error with token"
+            success: false,
+            message: "error with token"
         })
     }
 }
