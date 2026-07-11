ced657175dad3baf63885f44d725f3afd42ba2d1
Aditya <adityaguptavns1@gmail.com>
2026-07-11 23:43:47 +0530

    add conversation Schemag

--- src/schema/zodSchema.ts
+++ src/schema/zodSchema.ts
@@ -13,3 +13,7 @@ export const loginSchema = z.object({
     email : z.email(),
     password : z.string().min(6)
 })
+
+export const consversationAssignSchema = z.object({
+    agentId : z.string()
+})
